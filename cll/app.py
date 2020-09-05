34ef1a9b96bf8171c695aad7db1144adddf1dede
ajjr <ajjr@users.noreply.github.com>
2020-09-05 14:53:27 +0300

    Added a simple JSON api query for the lemma.

--- cll/app.py
+++ cll/app.py
@@ -1,3 +1,8 @@
+"""
+If this code were to be used in a live app it would have to
+be refactored to separate the routing from the logic.
+"""
+
 import flask
 from flask import request
 from flask import json
@@ -35,14 +40,17 @@ def index_post():
     text_data = request.form['text_data']
     language = request.form['language']
 
+    # Limit the input to max 1000 chars 
     if text_data.strip() == "" or len(text_data) > 1000:
         return flask.render_template('index.html', languages=languages)
 
+    # Construct the query as a dictionary for compatability with JSON
     text_qry = {'Content-Type': "text/plain",
                 'Content-Language': language,
                 'Payload': text_data}
     lemma = lemmatizer.tokenize(text_qry)
 
+    # Construct of JSON file for optional download
     filename = None
     with tempfile.NamedTemporaryFile(mode='w',
                                      dir=app.config['APP_DOWNLOAD_PATH'],
@@ -52,6 +60,7 @@ def index_post():
 
     filename_csv = filename.replace('.json', '.csv')
 
+    # The call to render_template should be abstracted away
     return flask.render_template('index.html', languages=languages,
                                  current_language=language,
                                  text_data=text_data,
@@ -62,17 +71,21 @@ def index_post():
 
 @app.route('/about')
 def about():
+    # Just a quick meta page
     return flask.render_template('about.html')
 
 
 @app.route('/download/<string:filename>')
 def download(filename: str):
+    # TODO: The logic should be separated from the route
     if filename.split('.')[-1] == 'json':
+        # The JSON file already exists, so just return it
         return flask.send_file('download/' + filename)
     else:
+        # We have to construct the CSV according to the JSON
         filename_json = filename.replace('.csv', '.json')
-        filename_csv = app.config['APP_DOWNLOAD_PATH'] + filename
-        with open(app.config['APP_DOWNLOAD_PATH'] + filename_json, 'r') as f:
+        filename_csv = app.config['APP_DOWNLOAD_PATH'] + '/' + filename
+        with open(app.config['APP_DOWNLOAD_PATH'] + '/' + filename_json, 'r') as f:
             data = json.load(f)
             columns = list(set([x for row in data for x in row.keys()]))
 
@@ -83,9 +96,23 @@ def download(filename: str):
                     csv_w.writerow(map(lambda x: elem.get(x, ""), columns))
                 return flask.send_file('download/' + filename)
 
+        # If we are here something, went wrong
         return flask.render_template('index.html', languages=languages,
                                      error="Failed to send data!")
 
+
+# JSON API should be as easy as this to implement:
+@app.route('/api/lemma', methods=['GET', 'POST'])
+def get_lemma():
+    str = request.args['q']
+    lemma = lemmatizer.tokenize(json.loads(str))
+    return flask.Response(json.dumps(lemma), mimetype='application/json')
+
+
+# JSON API language query:
+@app.route('/api/get_languages', methods=['GET'])
+def get_languages():
+    return flask.Response(json.dumps(languages), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
